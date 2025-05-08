d601e2e7404d114aad63f7a40fb4d85d804bc268
paullarasales <rkise2131@gmail.com>
2025-05-08 13:22:00 +0800

    added new migration

--- database/migrations/2025_05_08_051939_add_section_to_classes.php
+++ database/migrations/2025_05_08_051939_add_section_to_classes.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('classes', function (Blueprint $table) {
-            
+            $table->string('section');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('classes', function (Blueprint $table) {
-            //
+            $table->dropColumn('section');
         });
     }
 };
