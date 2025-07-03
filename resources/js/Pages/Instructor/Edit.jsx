e58890d69bc743bb6b4a186e0eda4a1dba6e65d5
paullarasales <rkise2131@gmail.com>
2025-07-03 13:12:18 +0800

    initial edit

--- resources/js/Pages/Instructor/Edit.jsx
+++ resources/js/Pages/Instructor/Edit.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useForm } from "@inertiajs/react";
 import InstructorLayout from "@/Layouts/InstructorLayout";
+import InputLabel from "@/Components/InputLabel";
+import InputError from "@/Components/InputError";
 
 export default function Edit({ classModel }) {
     const { data, setData, put, processing, errors } = useForm({
@@ -18,11 +20,10 @@ export default function Edit({ classModel }) {
         e.preventDefault();
         put(`/instructor/${classModel.id}`);
     };
-
     return (
         <InstructorLayout>
             <div className="h-form bg-white shadow-md rounded-md p-5">
-                <h1 className="text-2xl font-semibold" Edit Class></h1>
+                <h1 className="text-2xl font-semibold">Edit Class</h1>
                 <form
                     onSubmit={handleSubmit}
                     encType="multipart/form-data"
@@ -38,7 +39,42 @@ export default function Edit({ classModel }) {
                         </button>
                     </div>
                     <div className="h-[500px] w-full flex gap-4">
-                        <div className="h-[500px] w-1/2 p-4 flex flex-col gap-4 rounded"></div>
+                        <div className="h-[500px] w-1/2 p-4 flex flex-col gap-4 rounded">
+                            <div>
+                                <InputLabel
+                                    htmlFor="photo"
+                                    value="Upload New Photo (Optional)"
+                                />
+                                <input
+                                    type="file"
+                                    id="photo"
+                                    name="photo"
+                                    onChange={(e) =>
+                                        setData("photo", e.target.files[0])
+                                    }
+                                    className="w-full"
+                                />
+                                <InputError message={errors.photo} />
+
+                                <div>
+                                    <InputLabel
+                                        htmlFor="name"
+                                        value="Class Name"
+                                    />
+                                    <input
+                                        type="text"
+                                        id="name"
+                                        name="name"
+                                        value={data.name}
+                                        onChange={(e) =>
+                                            setData("name", e.target.value)
+                                        }
+                                        className="w-full border rounded px-3 py-2"
+                                    />
+                                    <InputError message={errors.name} />
+                                </div>
+                            </div>
+                        </div>
                         <div className="h-[500px] w-1/2 p-4 flex flex-col gap-4 rounded"></div>
                     </div>
                 </form>
